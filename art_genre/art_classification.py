d55fc2d2101039dccd10170c1e7f902983f987b0
DejiaTang <nora-tang@hotmail.com>
2018-04-27 01:16:36 +0000

    ..

--- art_genre/art_classification.py
+++ art_genre/art_classification.py
@@ -295,7 +295,7 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 #
 
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, 'model1.csv'
-                       num_epochs=epoch_num)
+                       ,num_epochs=epoch_num)
 
 ######################################################################
 ######################################################################
